968fdceb2452c3e5fe60366314e349f280a5b2ac
Juan Martin Gimenez <jm.gimenez.inf@gmail.com>
2017-05-30 10:31:58 -0300

    express OK! commit antes de instalar mongodb

---
+++ server/server.babel.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import React from 'react';
+
+const app = express();
+
+//** Levanta React
+app.use(express.static(__dirname + '/../build'));
+
+//** Levantar todos los routes (uno por uno)
+
+app.listen(3001, function () {
+  console.log('Servidor estático React-Express-Sequelize port 3001!');
+});
+
